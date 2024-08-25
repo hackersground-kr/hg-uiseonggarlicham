d12472c9ddc8d369b250c11d6e195a24699af9ba
bestdevmgp <pmg3858@icloud.com>
2024-08-26 04:20:41 +0900

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# memoa-server
---
+++ src/test/java/com/example/onna/OnnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
