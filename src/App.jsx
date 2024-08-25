728cbcfa844afbc7d94e2bf37d611e00005af953
joungminsung <joungminsung080830@gmail.com>
2024-08-26 02:22:35 +0900

    연결1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 // src/App.js
-import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/LoginPage/index';
 import Signup from './pages/SignUpPage/index'
 import Main from './pages/MainPage/index'
   
 function App() {
-  useEffect(() => {
-    // if not logged in navigate to login page
-  }, [])
   
   return (
     <BrowserRouter>
