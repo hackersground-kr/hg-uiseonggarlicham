0321a45cdb7f97a711a1cf2b28cfbf310bac5d90
dongwhi09 <dongwhi09@naver.com>
2024-08-26 18:00:51 +0900

    .

--- onna-server/src/test/java/com/example/onna/OnnaApplicationTests.java
+++ onna-server/src/test/java/com/example/onna/OnnaApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.onna;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest(classes = OnnaApplication.class)
 class OnnaApplicationTests {
 
     @Test
