aff8dccc03c6c675938233a25ffd7d58349b874d
joungminsung <164873464+joungminsung@users.noreply.github.com>
2024-08-27 10:46:14 +0900

    Update App.jsx

--- onna-web/src/App.jsx
+++ onna-web/src/App.jsx
@@ -4,8 +4,7 @@ import Login from './pages/LoginPage/index';
 import Signup from './pages/SignUpPage/index'
 import Main from './pages/MainPage/index';
 import Write from './pages/WritePage/index';
-
-import CalendarComponent  from './pages/Calendar/index';
+import CalendarComponent from './pages/Calendar/index';
 import Festival from './pages/FestivalPage/index';
 
 function App() {
